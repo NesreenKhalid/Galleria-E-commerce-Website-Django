2c3596d2934c1a72efa0e1818434cc75d4e63023
MennaAbdallah <menna.eid.95@gmail.com>
2020-03-06 01:49:32 +0200

    add Product, ProductPicture, and ProductComment models without FKs

--- E-commerce/src/product/admin.py
+++ E-commerce/src/product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from product.models import Product, ProductPicture, ProductComment
+
+admin.site.register(Product)
+admin.site.register(ProductPicture)
+admin.site.register(ProductComment)
--- E-commerce/src/product/models.py
+++ E-commerce/src/product/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    model = models.CharField(max_length=30)
+    review = models.TextField()
+    price = models.DecimalField(decimal_places=2,max_digits=6)
+    #BID = models.ForeignKey(Brand, on_delete=models.CASCADE)
+
+class ProductPicture(models.Model):
+    PID = models.ForeignKey(Product, on_delete=models.CASCADE)
+    whole_view = models.ImageField()
+    top_view = models.ImageField()
+    side_view = models.ImageField()
+    inner_view = models.ImageField()
+
+class ProductComment(models.Model):
+    PID = models.ForeignKey(Product, on_delete=models.CASCADE)
+    #UID = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+    review = models.DecimalField(decimal_places=1,max_digits=2)

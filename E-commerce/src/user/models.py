97e55802b9d26d64bacba092381c995ca5727424
NesreenKhalid <d.nesreenkhalid@gmail.com>
2020-03-06 07:32:09 +0200

    add cart model

--- E-commerce/src/user/models.py
+++ E-commerce/src/user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Cart(models.Model):
+    PID = models.ForeignKey('product.Product', on_delete=models.CASCADE , verbose_name='product ID')
+    #UID = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='user ID')
+    quantity = models.IntegerField(default=1)

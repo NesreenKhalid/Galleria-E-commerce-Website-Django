e7f6e316795f7c0e84fb7b739c73b75746e8c1f5
minanagy <minanagy1993@gmail.com>
2020-03-06 07:12:49 +0200

    Mina-Add 2 Class (Brand Table,Category Table)

--- E-commerce/src/product/models.py
+++ E-commerce/src/product/models.py
@@ -1,13 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+#mina start 
+class Category(models.Model):
+    CName=models.CharField(max_length=30,verbose_name=("Name of Category"))
+    CImage= models.ImageField(upload_to='product/',verbose_name=("Image"))
+
+    def __str__(self):
+        return self.CName
+
+class Brand(models.Model):
+    BRDName=models.CharField(max_length=30,verbose_name=("Name of Brand"))
+    BRDCategoryID = models.ForeignKey(Category, on_delete=models.CASCADE,verbose_name=("Category"))
+
+    def __str__(self):
+        return str(self.BRDCategoryID)
+
+#mina end
+
 class Product(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
     model = models.CharField(max_length=30)
     review = models.TextField()
     price = models.DecimalField(decimal_places=2,max_digits=6)
-    #BID = models.ForeignKey(Brand, on_delete=models.CASCADE)
+   # BID = models.ForeignKey(Brand, on_delete=models.CASCADE)
 
 class ProductPicture(models.Model):
     PID = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -21,3 +39,10 @@ class ProductComment(models.Model):
     #UID = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.TextField()
     review = models.DecimalField(decimal_places=1,max_digits=2)
+
+
+
+
+
+
+

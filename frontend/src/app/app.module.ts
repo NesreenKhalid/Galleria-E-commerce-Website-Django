15876d526c96a32f1775dcfb825979194f037de7
mofath <58465052+mofath@users.noreply.github.com>
2020-03-08 07:11:07 +0200

    user progile component added to fromtend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    UserProfileComponent,
+    WishlistComponent
   ],
   imports: [
     BrowserModule

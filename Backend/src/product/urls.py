93545d8ed0ba943bc7a535fa12079f5f98bd3160
Mina Nagy <minanagy1993@gmail.com>
2020-03-16 00:48:49 +0200

    Django Making queries(Filter Objects)

--- Backend/src/product/urls.py
+++ Backend/src/product/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .api import CategoryAPI
 from .api import BrandAPI
 from .api import ProductAPI
+from .api import Product_FilterAPI
+#from .api import Product_Filter_name_API
+
 from . import views 
 
 urlpatterns = [
@@ -9,4 +12,6 @@ urlpatterns = [
     path('category/',CategoryAPI.as_view()),
     path('Brand/',BrandAPI.as_view()),
     path('productlist/',ProductAPI.as_view()),
+    path('product/<int:num>/',Product_FilterAPI.as_view())
+    #path('product/(?P<username>.+)/$',Product_Filter_name_API.as_view())#Don't Work
 ]

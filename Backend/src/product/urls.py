f3ef0118c6d40230b3d3107e137c29b10ec33b28
MennaAbdallah <menna.eid.95@gmail.com>
2020-03-19 00:50:32 +0200

    GetProductById endpoint without ProductPictures

--- Backend/src/product/urls.py
+++ Backend/src/product/urls.py
@@ -4,6 +4,7 @@ from .api import BrandAPI
 from .api import ProductAPI
 from .api import Product_FilterAPI
 #from .api import Product_Filter_name_API
+from product.api import GetProductById
 
 from . import views 
 
@@ -12,6 +13,7 @@ urlpatterns = [
     path('category/',CategoryAPI.as_view()),
     path('Brand/',BrandAPI.as_view()),
     path('productlist/',ProductAPI.as_view()),
-    path('product/<int:num>/',Product_FilterAPI.as_view())
+    path('product/<int:num>/',Product_FilterAPI.as_view()),
     #path('product/(?P<username>.+)/$',Product_Filter_name_API.as_view())#Don't Work
+    path('product/id/<int:id>/',GetProductById.as_view())
 ]

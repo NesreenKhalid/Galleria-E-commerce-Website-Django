41d8a753527277120535a220956560b044a340ab
NesreenKhalid <d.nesreenkhalid@gmail.com>
2020-03-19 13:00:36 +0200

    fix error

--- Backend/src/product/urls.py
+++ Backend/src/product/urls.py
@@ -4,7 +4,7 @@ from .api import BrandAPI
 from .api import ProductAPI
 from .api import Product_FilterAPI
 #from .api import Product_Filter_name_API
-from product.api import GetProductById
+from .api import GetProductById
 
 from . import views 
 
@@ -13,7 +13,7 @@ urlpatterns = [
     path('category/',CategoryAPI.as_view()),
     path('Brand/',BrandAPI.as_view()),
     path('product/',ProductAPI.as_view()),
-    path('product/<int:num>/',Product_FilterAPI.as_view())
+    path('product/<int:num>/',Product_FilterAPI.as_view()),
     #path('product/(?P<username>.+)/$',Product_Filter_name_API.as_view())#Don't Work
     path('product/id/<int:id>/',GetProductById.as_view())
 ]
